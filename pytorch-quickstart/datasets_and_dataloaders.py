78d645bc6c014e4ed74cf49bdb0090e6af334caa
patrick starfish <patrickdulin02@gmail.com>
2026-09-23 17:00:36 +0800

    custom dataset
    custom dataset

--- pytorch-quickstart/datasets_and_dataloaders.py
+++ pytorch-quickstart/datasets_and_dataloaders.py
@@ -18,3 +18,33 @@ test_data = datasets.FashionMNIST(
     transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
 )
 
+
+#Iterating and Visualizing the Dataset
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+
+figure = plt.figure(figsize=(8,8))
+cols, rows = 3,3
+for i in range(1, cols * rows + 1):
+    sample_index = torch.randint(len(training_data), size=(1,)).item()
+    image, label = training_data[sample_index]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(image.squeeze(), cmap="gray")
+
+plt.show()
+
+
+#Creating a custom Dataset for you files
+
